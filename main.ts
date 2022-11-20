de9e8dc629554dab2a355a301c0c9cd6482a0050
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-11-20 07:36:44 +0600

    variable declaring

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
 // NO: 1
-function log ( message ){
+
+/* function log ( message ){
     console.log(message);
 }
 
 var message = 'Hello World';
-log(message)
+log(message) */
+
+// NO: 2 (declaring variable)
+
+function doSomething () {
+    for( var i = 0; i<5; i++ ){
+        console.log(i);
+    }
+    console.log('Finally' + i);
+}
+
+doSomething();

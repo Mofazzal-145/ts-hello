859161da2d41f24b5e7c16dfa67253759bb07f77
Md. Mofazzal Hossain <92473363+Mofazzal-145@users.noreply.github.com>
2022-12-31 18:38:32 +0600

    [variable declaring] different kind of variable

--- main.js
+++ main.js
@@ -6,10 +6,30 @@
 var message = 'Hello World';
 log(message) */
 // NO: 2 (declaring variable)
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
+/* function doSomething () {
+    for( var i = 0; i<5; i++ ){
         console.log(i);
     }
     console.log('Finally' + i);
 }
-doSomething();
+
+doSomething(); */
+// No : 3 (declaring variable)
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+;
+var backgroundCOlor = Color.Red;
--- main.ts
+++ main.ts
@@ -9,11 +9,30 @@ log(message) */
 
 // NO: 2 (declaring variable)
 
-function doSomething () {
+/* function doSomething () {
     for( var i = 0; i<5; i++ ){
         console.log(i);
     }
     console.log('Finally' + i);
 }
 
-doSomething();
+doSomething(); */
+
+// No : 3 (declaring variable)
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color { Red = 0, Green = 1, Blue = 2, Purple = 3 };
+let backgroundCOlor = Color.Red;
+
+
+
